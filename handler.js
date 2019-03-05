99c0d038e626a58b172c33090de0c547d7e4aba5
jacopo tagliabue <jacopo.tagliabue@tooso.org>
2019-03-04 17:55:47 -0800

    add more HTML elements - include JQuery and Chart.js first Gaussian Process charting

--- handler.js
+++ handler.js
@@ -7,9 +7,59 @@ module.exports.app = (event, context, callback) => {
 
     const html = `
     <html>
+    <head>
+        <title>WebPPL-to-AWS-lambda App</title>
+        <meta charset="UTF-8"> 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+        <link rel="icon" href="https://tooso.ai/assets/favicon.ico" />
+        <style>
+        </style>
+    </head>
     <body>
       <h1>WebPPL App</h1>
+      <div>
+        <input type="button" value="Probabilistic magic!" id="WebPPLBtn" />
+      </div>
+      <div>
+        <canvas id="gpPriorsCanvas" />
+      </div>
     </body>
+    <script>
+        var targetDiv = 'gpPriorsCanvas';
+        $("#WebPPLBtn" ).click(function() {
+            
+           $.getJSON("/dev/model", function(data) {
+                console.log("Drawing line chart with..." + data);
+                var chartData = [];
+                $.each(data['val'], function(index, series) {
+                     chartData.push({
+                            label: "Sample " + (index + 1),
+                            fill: false,
+                            borderColor: 'rgb(' + [randomRGB(), randomRGB(), randomRGB()].join() + ')',
+                            data: series
+                        })
+                    });
+                drawLineChart(chartData, targetDiv);
+            });
+        });
+        
+        var randomIntInRange = function(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        };
+        
+        var randomRGB = function() {
+            return randomIntInRange(0, 256);
+        };
+        
+        var drawLineChart = function(data, targetDiv) {
+            var ctx = document.getElementById(targetDiv).getContext('2d');
+            var chart = new Chart(ctx, {
+                type: 'scatter',
+                data: { datasets: data }
+            });
+        };
+    </script>
     </html>`;
 
     const response = {
@@ -35,19 +85,30 @@ module.exports.model = (event, context, callback) => {
     console.log(event);
 
     const code = `
-        var roll = Infer({
-          method: 'enumerate',
-          model() {
-            var die1 = randomInteger(6) + 1;
-            var die2 = randomInteger(6) + 1;
+        var N_SERIES = 4;
         
-            return die1 + die2;
-          }
-        });
+        var radialBasisKernel = function(x, y) {
+            return Math.exp(-0.5 * Math.pow(x - y, 2));
+        }
         
-        map(function(x) {
-          return {x: x, p: Math.exp(roll.score(x))};
-        }, roll.support());`;
+        var applyKernelFunction = function(xa, xb, kernelFunction) {
+            return mapN(function(y) { mapN(function(i) { return kernelFunction(xa[y], xb[i]); }, xb.length)}, xa.length)
+        };
+        
+        var zeroVector = function(size) {
+          return Vector(repeat(size, function(x) { return 0.0; }));
+        }
+        
+        var xs = _.range(-4,4.1, 0.5);
+        var covTensor = Matrix(applyKernelFunction(xs, xs, radialBasisKernel));
+        var ys = repeat(N_SERIES, function(x) { return sample(MultivariateGaussian({mu: zeroVector(xs.length), cov: covTensor})); });
+        
+        // for each tensor get just the series data; for each series data (a dictionary index: value), get values
+        // result is a list of list, each list is the y vals mapping to the xs specified
+        var yVals = map(function(series) { return mapN(function(n) { return series[n]; }, Object.keys(series).length)}, map(function(series) { return series.data; }, ys));
+        
+        // finally, for each series print out the pairs x, y
+        map(function(ySeries) { return mapN(function(n) { return {x: xs[n], y: ySeries[n]}; }, xs.length)}, yVals);`;
 
     // running webppl code
     webppl.run(code, (s, val) => {
